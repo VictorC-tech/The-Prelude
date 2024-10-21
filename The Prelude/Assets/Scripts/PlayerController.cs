04e5799491065a07cb8514b2eac525b9a276c44a
Tidlle <98828582+Tidlle@users.noreply.github.com>
2024-10-21 10:07:12 -0300

    move

---
+++ The Prelude/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D _playerRigidbody2D;
+
+    [SerializeField] 
+    private float _playerSpeed;
+
+    private Vector2 _playerDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        _playerRigidbody2D.MovePosition(_playerRigidbody2D.position + _playerDirection * _playerSpeed * Time.fixedDeltaTime);
+    }
+}
