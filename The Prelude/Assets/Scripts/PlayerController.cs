28808a171c6978ed8b1394edf729a95e514eb9e1
Tidlle <98828582+Tidlle@users.noreply.github.com>
2024-10-21 11:28:08 -0300

    move_animation

--- The Prelude/Assets/Scripts/PlayerController.cs
+++ The Prelude/Assets/Scripts/PlayerController.cs
@@ -6,21 +6,29 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody2D _playerRigidbody2D;
-
-    [SerializeField] 
+    private Animator _playerAnimator;
     private float _playerSpeed;
-
     private Vector2 _playerDirection;
     // Start is called before the first frame update
     void Start()
     {
         _playerRigidbody2D = GetComponent<Rigidbody2D>();
+        _playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         _playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (_playerDirection.sqrMagnitude > 0 )
+        {
+            _playerAnimator.SetInteger("Movimento", 1);
+        }
+        else
+        {
+            _playerAnimator.SetInteger("Movimento", 0);
+        }
     }
 
     void FixedUpdate()
