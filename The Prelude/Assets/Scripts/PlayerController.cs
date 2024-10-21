5ee67bd110d7e2c33e1383bd1aab587e5fcf42cb
Reimar <98828582+Tidlle@users.noreply.github.com>
2024-10-21 13:32:31 -0300

    move-finish

--- The Prelude/Assets/Scripts/PlayerController.cs
+++ The Prelude/Assets/Scripts/PlayerController.cs
@@ -7,19 +7,24 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D _playerRigidbody2D;
     private Animator _playerAnimator;
+    [SerializeField]
     private float _playerSpeed;
+    private float _playerInitialSpeed;
+    public float _playerRunSpeed;
     private Vector2 _playerDirection;
     // Start is called before the first frame update
     void Start()
     {
         _playerRigidbody2D = GetComponent<Rigidbody2D>();
         _playerAnimator = GetComponent<Animator>();
+        _playerInitialSpeed = _playerSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
         _playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _playerDirection.Normalize();
 
         if (_playerDirection.sqrMagnitude > 0 )
         {
@@ -29,10 +34,32 @@ public class PlayerController : MonoBehaviour
         {
             _playerAnimator.SetInteger("Movimento", 0);
         }
+        Flip();
+
     }
 
     void FixedUpdate()
     {
         _playerRigidbody2D.MovePosition(_playerRigidbody2D.position + _playerDirection * _playerSpeed * Time.fixedDeltaTime);
+    }
+
+    void Flip()
+    {
+        if (_playerDirection.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 0f);
+        }
+        else if (_playerDirection.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 180f);
+        }
+    }
+    
+    void PlayerRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _player
+        }
     }
 }
